da5603efbeff36785320ed6a8d1e72ce2484457c
kiko <95848578+K1K04@users.noreply.github.com>
2024-05-29 13:21:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect , url_for, jsonify
 import json
 
 app = Flask(__name__)
